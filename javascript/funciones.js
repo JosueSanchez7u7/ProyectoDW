183429021af63cf244cbc476d3f904ca9a153c8f
JosueSanchez7u7 <josue.sanchez10@live.com>
2021-05-26 15:41:22 -0600

    Agregue la funcion para aggLugares y busqueda

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -1,11 +1,30 @@
 //Lista que contiene los lugares turisticos
 var ListTurismo = [];
-/*
-function ingresarNom(){
-    nombre.push({nombres:document.getElementById("nombres").value});
-    console.log(nombre)
+//Para agg datos en el div con ID tablaUsers
+//var aggTablaTurismo = document.getElementById("tablaUsers");
+
+//Impresion de datos ingresados por el usuario en el html
+var tablaTurismoUser =  `<table id="tTurismoUser">
+                            <tr>
+                                <th>Nombre del lugar</th><th>Pais</th><th>Descripcion</th><th>Direccion</th>
+                            </tr>
+                            </table>`
+
+function verTablaTurismoUser(){
+    //console.log("Entre a la funcion ver tabla turismo user");
+    //console.log(aggTablaTurismo);
+    document.getElementById("tablaUsers").innerHTML = tablaTurismoUser; 
+    //aggTablaTurismo.innerHTML = tablaTurismoUser;
+    mostrarListUsers();
 }
-*/
+
+function LimpiarTabla(){
+    //console.log("Entre a la funcion ver tabla turismo user");
+    //console.log(aggTablaTurismo);
+    document.getElementById("tablaUsers").innerHTML = tablaTurismoUser; 
+    //aggTablaTurismo.innerHTML = tablaTurismoUser;
+    //mostrarListUsers();
+}
 
 //Funcion para agregar lugares turisticos desde la pagina 3
 function aggTurismo(){
@@ -21,6 +40,45 @@ function aggTurismo(){
         ListTurismo.push({nombreLugar:tnombre,pais:tpais,descripcion:tdescripcion,direccion:tdireccion,tipo:tTipo});
         localStorageListTurismo(ListTurismo);
     }
+}
+
+function mostrarBusqueda(i){
+    // Metodo recursivo para mostrar resultados de busqueda
+    console.log("Mostrando el resultado de la busqueda");
+    var bTurismo = document.getElementById("rBusqueda");
+    var List = getListTurismo();
+    bTurismo.innerHTML += `<tr>
+                                <td>${List[i].nombreLugar}</td>
+                                <td>${List[i].pais}</td>
+                                <td>${List[i].descripcion}</td>
+                                <td>$${List[i].direccion}</td>
+                                </tr>`;
+}
+
+function buscarTurismo(){
+    //Asignando los valores que traen los formularios para realizar la busqueda
+    //var bTurismo = document.getElementById("rBusqueda");
+    console.log("Entre a la funcion buscar Turismo");
+    var List = getListTurismo();
+    var bNombre = document.getElementById("bnombre").value;
+    var bPais = document.getElementById("bpais").value;
+    
+    //Validando parametros
+    if(bNombre != "" && bPais != ""){
+        //Recorrido por la lista de lugares turisticos
+        for(i in List){
+            //Condicion de la busqueda
+            if(bNombre == List[i].nombreLugar && bPais == List[i].pais){
+                //Se encontro el objeto que cumple con la condicion de busqueda
+
+                mostrarBusqueda(i);
+            } else{
+                //No se encontro ninguna coincidencia
+
+            }
+        }
+    }
+    
 }
 
 //Funcion para devolver los valores en el almacenamiento local
@@ -39,4 +97,21 @@ function getListTurismo(){
 //Funcion para el almacenamiento local
 function localStorageListTurismo(pList){
     localStorage.setItem("TurismoUsuario", JSON.stringify(pList));
+}
+
+//Funcion para mostrar la lista de lugares turisticos ingresados por usuarios
+function mostrarListUsers(){
+    var List = getListTurismo();
+    var tListUser = document.getElementById("tTurismoUser");
+
+    for(i in List){
+        if(List[i].tipo == 1){
+            tListUser.innerHTML += `<tr>
+                               <td>${List[i].nombreLugar}</td>
+                               <td>${List[i].pais}</td>
+                               <td>${List[i].descripcion}</td>
+                               <td>${List[i].direccion}</td>
+                               </tr>`;
+        }
+    }
 }
